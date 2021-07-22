25c194e9a79de861f489c1a18cf09ced5d97a28d
aman deo <amanhungry47@gmail.com>
2021-07-22 15:18:50 +0530

    uploading data and data collection script

--- untitled25.py
+++ untitled25.py
@@ -8,4 +8,5 @@ Created on Wed Jul 21 22:11:33 2021
 import glassdoor_scraper as gs
 import pandas as pd
 path = "C:/Users/ASUS/Documents/ds_salary_proj/chromedriver"
-df = gs.get_jobs('data scientist', 15, False, path, 15)    
+df = gs.get_jobs('data scientist', 100 , False, path, 15)    
+df.to_csv('Glassdoor_jobs.csv', index = False)
